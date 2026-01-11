eb80f51553b909885e596696c5c3ee568ba0904b
dickmao <none>
2026-01-11 09:32:46 -0500

    retag.sh

---
+++ retag.sh
@@ -0,0 +1,5 @@
+export VERSION=`git describe --tags --abbrev=0 2>/dev/null || echo v1`
+2>/dev/null git tag -d $VERSION || true
+2>/dev/null git push --delete origin $VERSION || true
+git tag $VERSION
+git push origin $VERSION
